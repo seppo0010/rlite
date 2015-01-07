0cb88332f81f3e0ea131b69b2b98d51ac7d4ead2
Sebastian Waisbrot <seppo0010@gmail.com>
2015-01-07 11:02:22 -0800

    Add RPOP

--- src/hirlite.c
+++ src/hirlite.c
@@ -1972,6 +1972,10 @@ cleanup:
 	return;
 }
 
+static void rpopCommand(rliteClient *c) {
+	popGenericCommand(c, 0);
+}
+
 static void lpopCommand(rliteClient *c) {
 	popGenericCommand(c, 1);
 }
@@ -2158,7 +2162,7 @@ struct rliteCommand rliteCommandTable[] = {
 	// {"rpushx",rpushxCommand,3,"wmF",0,NULL,1,1,1,0,0},
 	{"lpushx",lpushxCommand,3,"wmF",0,1,1,1,0,0},
 	// {"linsert",linsertCommand,5,"wm",0,NULL,1,1,1,0,0},
-	// {"rpop",rpopCommand,2,"wF",0,NULL,1,1,1,0,0},
+	{"rpop",rpopCommand,2,"wF",0,1,1,1,0,0},
 	{"lpop",lpopCommand,2,"wF",0,1,1,1,0,0},
 	// {"brpop",brpopCommand,-3,"ws",0,NULL,1,1,1,0,0},
 	// {"brpoplpush",brpoplpushCommand,4,"wms",0,NULL,1,2,1,0,0},
