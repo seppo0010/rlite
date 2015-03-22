d4aafaa55d01f3b2c89c72cc9123ccdf9c4426af
Sebastian Waisbrot <seppo0010@gmail.com>
2015-03-21 20:20:21 -0700

    Dat english

--- src/pubsub.h
+++ src/pubsub.h
@@ -7,7 +7,7 @@
 int rl_subscribe(rlite *db, int channelc, unsigned char **channelv, long *channelvlen);
 int rl_poll(rlite *db, int *elementc, unsigned char ***_elements, long **_elementslen);
 /**
- * When timeout is NULL, it will wait until for ever.
+ * When timeout is NULL, it will wait for ever.
  */
 int rl_poll_wait(rlite *db, int *elementc, unsigned char ***_elements, long **_elementslen, struct timeval *timeout);
 int rl_publish(rlite *db, unsigned char *channel, size_t channellen, const char *data, size_t datalen, long *recipients);
