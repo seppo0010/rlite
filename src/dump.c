25d579f0f7b7d2c6db56385dd6f03f226370a3cb
Sebastian Waisbrot <seppo0010@gmail.com>
2015-09-10 19:21:28 -0700

    Move doc block to header file

--- src/dump.c
+++ src/dump.c
@@ -231,12 +231,6 @@ cleanup:
 	return retval;
 }
 
-/**
- * Dumps the content of `key` into `data`.
- *
- * It will contain a two bytes of a redis-compatible dump version,
- * and a crc32 checksum at the end.
- */
 int rl_dump(struct rlite *db, const unsigned char *key, long keylen, unsigned char **data, long *datalen)
 {
 	int retval;
--- src/rlite/dump.h
+++ src/rlite/dump.h
@@ -1,3 +1,9 @@
 #include "rlite.h"
 
+/**
+ * Dumps the content of `key` into `data`.
+ *
+ * It will contain a two bytes of a redis-compatible dump version,
+ * and a crc32 checksum at the end.
+ */
 int rl_dump(struct rlite *db, const unsigned char *key, long keylen, unsigned char **data, long *datalen);
